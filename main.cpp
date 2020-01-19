85c65c625269338e01150a585e4d8d62e2678b8c
kuba66714 <57587967+kuba66714@users.noreply.github.com>
2020-01-19 19:31:35 +0100

    przechodzi przyklad

--- main.cpp
+++ main.cpp
@@ -2,27 +2,107 @@
 #include <unordered_map>
 #include <regex>
 #include <list>
-class Mode {
+#include <random>
+
+class PlayerException : public std::exception{
+public:
+    const char* what() const noexcept override = 0;
 };
-class SequenceMode : public Mode {
+
+class WrongType : public PlayerException {
+public:
+    const char* what() const noexcept override {
+        return "Zly typ pliku";
+    }
 };
-class OddEvenMode : public Mode {
+class WrongLyrics : public PlayerException {
+public:
+    const char* what() const noexcept override {
+        return "Zly format slow";
+    }
 };
-class ShuffleMode : public Mode {
+class NoNecessaryData : public PlayerException {
+public:
+    const char* what() const noexcept override {
+        return "Brak potrzebnych danych";
+    }
 };
-SequenceMode createSequenceMode() {
-    return SequenceMode();
-}
-OddEvenMode createOddEvenMode() {
-    return OddEvenMode();
-}
-ShuffleMode createShuffleMode() {
-    return ShuffleMode();
-}
 class PlaylistInterface {
 public:
     virtual void play() = 0;
 };
+
+class Mode {
+public:
+    virtual void play_with_mode(std::list<PlaylistInterface*>& list) {
+
+    }
+};
+class SequenceMode : public Mode {
+public:
+    void play_with_mode(std::list<PlaylistInterface*>& list) override;
+};
+
+void SequenceMode::play_with_mode(std::list<PlaylistInterface *> &list) {
+    std::list<PlaylistInterface *>::iterator it;
+    for (it = list.begin(); it != list.end(); it++) {
+        (*it)->play();
+    }
+}
+
+class OddEvenMode : public Mode {
+public:
+    void play_with_mode(std::list<PlaylistInterface*>& list) override;
+};
+void play_every_two(std::list<PlaylistInterface *>::iterator& it, std::list<PlaylistInterface *>& list) {
+    while (it != list.end()) {
+        (*it)->play();
+        it++;
+        if (it != list.end()) {
+            it++;
+        }
+    }
+}
+
+void OddEvenMode::play_with_mode(std::list<PlaylistInterface *>& list) {
+    std::list<PlaylistInterface *>::iterator it;
+    it = list.begin();
+    it++;
+    play_every_two(it, list);
+    it = list.begin();
+    play_every_two(it, list);
+}
+
+class ShuffleMode : public Mode {
+private:
+    size_t seed;
+public:
+    ShuffleMode(size_t new_seed) {
+        seed = new_seed;
+    }
+    void play_with_mode(std::list<PlaylistInterface*>& list) override;
+};
+
+void ShuffleMode::play_with_mode(std::list<PlaylistInterface *>& list) {
+    std::vector<std::reference_wrapper<PlaylistInterface *>> vec(list.begin(), list.end());
+    std::shuffle(vec.begin(), vec.end(), std::default_random_engine(seed));
+    std::list<PlaylistInterface*> new_list {vec.begin(), vec.end()};
+    std::list<PlaylistInterface *>::iterator it;
+    for (it = new_list.begin(); it != new_list.end(); it++) {
+        (*it)->play();
+    }
+}
+
+SequenceMode* createSequenceMode() {
+    return new SequenceMode();
+}
+OddEvenMode* createOddEvenMode() {
+    return new OddEvenMode();
+}
+ShuffleMode* createShuffleMode(size_t seed) {
+    return new ShuffleMode(seed);
+}
+
 class Playlist : public PlaylistInterface {
 private:
     std::list<PlaylistInterface*> list_to_play;
@@ -32,17 +112,15 @@ public:
     Playlist(const char* myname) {
         list_to_play = std::list<PlaylistInterface*>();
         name = myname;
-        SequenceMode sm;
-        mode = &sm;
+        SequenceMode* sm = new SequenceMode();
+        mode = sm;
     }
     void add(PlaylistInterface* pi);
     void add(PlaylistInterface* pi, size_t position);
     void remove();
     void remove(size_t position);
-    void setMode(Mode& mode);
-    std::list<PlaylistInterface*>& getter() {
-        return list_to_play;
-    }
+    void setMode(Mode* mode);
+
     void play() override;
 };
 
@@ -67,16 +145,13 @@ void Playlist::remove(size_t position) {
     list_to_play.erase(it);
 }
 
-void Playlist::setMode(Mode& new_mode) {
-    mode = &new_mode;
+void Playlist::setMode(Mode* new_mode) {
+    mode = new_mode;
 }
 
 void Playlist::play() {
     std::cout<<"Playlist ["<<name<<"]"<<std::endl;
-    std::list<PlaylistInterface*>::iterator it;
-    for (it = list_to_play.begin(); it != list_to_play.end(); it++) {
-        (*it)->play();
-    }
+    mode->play_with_mode(list_to_play);
 }
 
 class Play : public PlaylistInterface {
@@ -94,13 +169,13 @@ public:
     Song(std::unordered_map<std::string, std::string>& data, std::string& lyrics_add) {
         auto it = data.find("artist");
         if (it == data.end()) {
-            //BLAD
+            throw NoNecessaryData();
         } else {
             artist = it->second;
         }
         it = data.find("title");
         if (it == data.end()) {
-            //BLAD
+            throw NoNecessaryData();
         } else {
             title = it->second;
         }
@@ -122,13 +197,13 @@ public:
     Movie(std::unordered_map<std::string, std::string>& data, std::string& lyr) {
         auto it = data.find("year");
         if (it == data.end()) {
-            //BLAD
+            throw NoNecessaryData();
         } else {
             year = it->second;
         }
         it = data.find("title");
         if (it == data.end()) {
-            //BLAD
+            throw NoNecessaryData();
         } else {
             title = it->second;
         }
@@ -138,7 +213,7 @@ public:
 };
 
 void Movie::play() {
-    std::cout<<"Song ["<<title<<" "<<year<<"]: "<<lyrics<<std::endl;
+    std::cout<<"Movie ["<<title<<" "<<year<<"]: "<<lyrics<<std::endl;
 }
 
 class File {
@@ -170,8 +245,7 @@ void File::parse(std::string &str) {
     std::smatch m;
     std::regex e1("^(audio|video)\\|");
     if (!std::regex_search(str, m, e1)) {
-        //BLAD
-        return;
+        throw WrongType();
     }
     file_type = m.str(0);
     file_type = file_type.substr(0, file_type.size() - 1);
@@ -194,7 +268,7 @@ void File::parse(std::string &str) {
     if (std::regex_match(str, m, e4)) {
         lyrics = str;
     } else {
-        //BLAD
+        throw WrongLyrics();
     }
 }
 
@@ -238,9 +312,6 @@ Play* Player::openFile(File file) {
     } else if (file.get_file_type() == "video") {
         MovieFactory mf = MovieFactory();
         play = mf.create_play(file);
-    } else {
-
-        //BLAD
     }
     return play;
 }
@@ -255,8 +326,6 @@ int main() {
 
     auto mishmash = player.createPlaylist("mishmash");
     auto armstrong = player.createPlaylist("armstrong");
-    File f = File("audio|artist:Louis Armstrong|title:What a Wonderful World|"
-                  "I see trees of green, red roses too...");
     auto whatAWonderfulWorld = player.openFile(File("audio|artist:Louis Armstrong|title:What a Wonderful World|"
                                                     "I see trees of green, red roses too..."));
     auto helloDolly = player.openFile(File("audio|artist:Louis Armstrong|title:Hello, Dolly!|"
@@ -275,6 +344,39 @@ int main() {
 
     std::cout << "=== Playing 'mishmash' (default sequence mode)" << std::endl;
     mishmash->play();
+
+    std::cout << "=== Playing 'mishmash' (shuffle mode, seed 0 for std::default_random_engine)" << std::endl;
+    mishmash->setMode(createShuffleMode(0));
+    mishmash->play();
+
+    std::cout << "=== Playing 'mishmash' (removed cabaret and last direstraits, odd-even mode)" << std::endl;
+    mishmash->remove(0);
+    mishmash->remove();
+    mishmash->setMode(createOddEvenMode());
+    mishmash->play();
+
+    std::cout << "=== Playing 'mishmash' (sequence mode, 'armstrong' odd-even mode)" << std::endl;
+    armstrong->setMode(createOddEvenMode());
+    mishmash->setMode(createSequenceMode());
+    mishmash->play();
+
+    try {
+        auto unsupported = player.openFile(File("mp3|artist:Unsupported|title:Unsupported|Content"));
+    } catch (PlayerException const& e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    try {
+        auto corrupted = player.openFile(File("Corrupt"));
+    } catch (PlayerException const& e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    try {
+        auto corrupted = player.openFile(File("audio|artist:Louis Armstrong|title:Hello, Dolly!|%#!@*&"));
+    } catch (PlayerException const& e) {
+        std::cout << e.what() << std::endl;
+    }
 
     return 0;
 }
